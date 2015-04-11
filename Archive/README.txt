02630bd77c9c1c29c607e1719176d292a2ff802f
nazik <nazikhuq@yahoo.com>
2015-04-11 11:35:07 -0400

    Updated prerequisites.

--- Archive/README.txt
+++
@@ -1,3 +0,0 @@
-﻿For a video information on how to use this project template, please see this blog post:
-
-http://www.bradygaster.com/post/announcing-the-management-libraries-for-net-general-availability
--- Program.cs
+++ Program.cs
@@ -19,20 +19,31 @@ namespace ManagementLibrariesDemo
 {
     class Program
     {
-        // Prerequisites
-        // 1.   Download the following credential file .publishsettings which has a Base64 azure management certificate from this link https://manage.windowsazure.com/publishsettings
-        //      You will need Azure Subscription for the above link to work. You can sign up for one from this link http://azure.microsoft.com/en-us/pricing/free-trial/
-        //     
-        // 2.   Reference Nuget Microsoft Azure Automation Management Library using the Visual Sudio 2013 Package Manger Console (navigate to Tools/Nuget Package Manager/Package Manager Console)
-        //      Run this command "Install-Package Microsoft.WindowsAzure.Management.Automation" from the Package Manager Console.
 
-        
+        //Prerequisites
+
+        //1.Visual Studio 2013 Community Edition or above https://www.visualstudio.com/en-us/products/visual-studio-community-vs
+
+
+        //2.Download the following credential file .publishsettings which has the Base64 azure management certificate from this link 
+        //  https://manage.windowsazure.com/publishsettings. You will need an Azure Subscription to download the file. You can sign 
+        //  up for one from this link http://azure.microsoft.com/en-us/pricing/free-trial/ .
+
+
+        //3.Reference Nuget "Microsoft Azure Automation Management Library".
+
+        //Right-click on the solution and select "Enable Nuget Package Retore".
+
+        //Or
+
+        //Use the Visual Sudio 2013 Package Manger Console (navigate to Tools/Nuget Package Manager/Package Manager Console). 
+        //Then run this command "Install-Package Microsoft.WindowsAzure.Management.Automation" from the Package Manager Console.
 
         static string credentialsPath = @"[C:\MyFiles\AzureCredentialFile.publishsettings]";    // Reference the file .publishesetting file downloaded from the link above.
-        static string subscriptionId = "[Subscription Id]";                  // Copy this from Azure portal under settings area
+        static string subscriptionId = "[Subscription Id]";                                     // Copy this from Azure portal under settings area
 
         static string Base64cer;
-        static string subscriptionName = "[Subscription name]";                                                    // Name of the Azure subscription
+        static string subscriptionName = "[Subscription name]";                                 // Optional(You can keep it as is). Name of the Azure subscription
         static void Main(string[] args)
         {
             RunDemos();
